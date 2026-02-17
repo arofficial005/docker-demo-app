71a088119a61605cafed2fe6188d4f21f9328b27
CodeNova <100618080+arofficial005@users.noreply.github.com>
2026-02-17 11:52:32 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -19,3 +19,5 @@ async def read_root(request: Request):
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+#new ID added
